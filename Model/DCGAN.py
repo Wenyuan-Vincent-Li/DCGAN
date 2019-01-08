84d763e7b07ac075988c4b00afffb2f53b5098b8
wenyuan <liwenyuan.zju@gmail.com>
2019-01-08 11:31:41 -0800

    update mnist without condition

--- Model/DCGAN.py
+++ Model/DCGAN.py
@@ -47,14 +47,14 @@ class DCGAN(model_base.GAN_Base):
 
             else: ## use conditional GAN
                 if self.config.DATA_NAME == "mnist":
-                    yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES]) ## [None, 1, 1, 10]
-                    z = tf.concat([z, y], 1) # concat the z and y in the latent space
+                    # yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES]) ## [None, 1, 1, 10]
+                    # z = tf.concat([z, y], 1) # concat the z and y in the latent space
 
                     ## first linear layer
                     h0 = self._linear_fc(z, 1024, 'g_h0_lin')
                     h0 = self._batch_norm_contrib(h0, 'g_bn0', train = True)
                     h0 = tf.nn.relu(h0, 'g_rl0')
-                    h0 = tf.concat([h0, y], 1)
+                    # h0 = tf.concat([h0, y], 1)
 
                     ## second linear layer
                     h1 = self._linear_fc(h0, self.config.BATCH_SIZE * 2 * 7 * 7, 'g_h1_lin')
@@ -63,13 +63,13 @@ class DCGAN(model_base.GAN_Base):
 
                     ## reshape to conv feature pack and concat with label condition
                     h1 = tf.reshape(h1, [self.config.BATCH_SIZE, 7, 7, 64 * 2])
-                    h1 = self._conv_cond_concat(h1, yb)
+                    # h1 = self._conv_cond_concat(h1, yb)
 
                     ## first layer deconv
                     h2 = self._deconv2d(h1, 128, name = 'g_dconv0')
                     h2 = self._batch_norm_contrib(h2, 'g_bn2', train = True)
                     h2 = tf.nn.relu(h2, 'g_rl2')
-                    h2 = self._conv_cond_concat(h2, yb)
+                    # h2 = self._conv_cond_concat(h2, yb)
 
                     ## output layer: sigmoid to map the data range to [0, 1]
                     h3 = self._deconv2d(h2, 1, name = 'g_dconv1')
@@ -139,13 +139,13 @@ class DCGAN(model_base.GAN_Base):
 
             else:
                 if self.config.DATA_NAME == "mnist":
-                    yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES])
-                    x = self._conv_cond_concat(image, yb)
+                    # yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES])
+                    # image = self._conv_cond_concat(image, yb)
 
                     # first conv
-                    h0 = self._conv2d(x, 1 + self.config.NUM_CLASSES, name = 'd_h0_conv')
+                    h0 = self._conv2d(image, 1 + self.config.NUM_CLASSES, name = 'd_h0_conv')
                     h0 = tf.nn.leaky_relu(h0, alpha = 0.2, name = 'd_leaky0')
-                    h0 = self._conv_cond_concat(h0, yb)
+                    # h0 = self._conv_cond_concat(h0, yb)
 
                     # second conv
                     h1 = self._conv2d(h0, 64 + self.config.NUM_CLASSES, name = 'd_h1_conv')
@@ -155,13 +155,13 @@ class DCGAN(model_base.GAN_Base):
                     fm = h1
                     # reshape and concat the label
                     h1 = tf.reshape(h1, [self.config.BATCH_SIZE, -1])
-                    h1 = tf.concat([h1, y], 1)
+                    # h1 = tf.concat([h1, y], 1)
 
                     ## fc layer
                     h2 = self._linear_fc(h1, 1024, 'd_h2_lin')
                     h2 = self._batch_norm_contrib(h2, name = 'd_h2_bn', train = True)
                     h2= tf.nn.leaky_relu(h2, alpha = 0.2, name = 'd_leaky2')
-                    h2 = tf.concat([h2, y], 1)
+                    # h2 = tf.concat([h2, y], 1)
                     if self.config.MINIBATCH_DIS:
                         f = self._minibatch_discrimination(h2, 100)
                         h2 = tf.concat([h2, f], 1)
@@ -244,14 +244,14 @@ class DCGAN(model_base.GAN_Base):
                 return h4
             else:
                 if self.config.DATA_NAME == "mnist":
-                    yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES])  ## [None, 1, 1, 10]
-                    z = tf.concat([z, y], 1)  # concat the z and y in the latent space
+                    # yb = tf.reshape(y, [self.config.BATCH_SIZE, 1, 1, self.config.NUM_CLASSES])  ## [None, 1, 1, 10]
+                    # z = tf.concat([z, y], 1)  # concat the z and y in the latent space
 
                     ## first linear layer
                     h0 = self._linear_fc(z, 1024, 'g_h0_lin')
                     h0 = self._batch_norm_contrib(h0, 'g_bn0', train = False)
                     h0 = tf.nn.relu(h0, 'g_rl0')
-                    h0 = tf.concat([h0, y], 1)
+                    # h0 = tf.concat([h0, y], 1)
 
                     ## second linear layer
                     h1 = self._linear_fc(h0, self.config.BATCH_SIZE * 2 * 7 * 7, 'g_h1_lin')
@@ -260,13 +260,13 @@ class DCGAN(model_base.GAN_Base):
 
                     ## reshape to conv feature pack and concat with label condition
                     h1 = tf.reshape(h1, [self.config.BATCH_SIZE, 7, 7, 64 * 2])
-                    h1 = self._conv_cond_concat(h1, yb)
+                    # h1 = self._conv_cond_concat(h1, yb)
 
                     ## first layer deconv
                     h2 = self._deconv2d(h1, 128, name='g_dconv0')
                     h2 = self._batch_norm_contrib(h2, 'g_bn2', train = False)
                     h2 = tf.nn.relu(h2, 'g_rl2')
-                    h2 = self._conv_cond_concat(h2, yb)
+                    # h2 = self._conv_cond_concat(h2, yb)
 
                     ## output layer: sigmoid to map the data range to [0, 1]
                     h3 = self._deconv2d(h2, 1, name='g_dconv1')
